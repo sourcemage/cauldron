51ac95c248894b12ea6e29335c942a05cabcfbd8
Karsten Behrmann <bearperson@gmx.net>
2005-09-29 21:28:10 -0100

    Might as well call it a new version by now

--- misc/release.sh
+++ misc/release.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-p4 edit ${0%/*}/../iso/data/internal_version
+p4 edit $(readlink -f ${0%/*}/../iso/data/internal_version)
+# p4 can't cope with foo/../bar
 date -u +%Y%m%d%H >${0%/*}/../iso/data/internal_version
