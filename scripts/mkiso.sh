9933c9767820c842c039581aedaea263575b414e
Justin Boffemmyer <flux@sourcemage.org>
2008-10-08 22:39:12 -0400

    scripts/mkiso.sh: makes iso image from chroot
    This script takes 2 arguments and produces and ISO image file
    (compressed or not depending on options) named with version ($2) from
    a given iso chroot directory ($1).

---
+++ scripts/mkiso.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COMPRESS="no"
+KEEP=
+
+while getopts "z" Options
+do
+	case $Options in
+		"z") COMPRESS="yes"
+		"k") KEEP="-k"
+	esac
+done
+shift $(($OPTIND - 1))
+
+ISO_CHROOT=$1
+ISO_VERSION=$2
+
+mkisofs -R -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V SOURCEMAGE -o "smgl-${ISO_VERSION}.iso" "$ISO_CHROOT"
+
+if [[ "$COMPRESS" == yes || -n "$KEEP" ]]
+then
+	bzip2 -f -v $KEEP "${ISO_VERSION}.iso"
+fi
+
