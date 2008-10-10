c548d76d64e3c03725704cccfdefd96133aacb8a
Justin Boffemmyer <flux@sourcemage.org>
2008-10-09 20:52:58 -0400

    scripts/mkiso.sh: don't force smgl- in iso name
    Made it so that the entire ISO name is configured by the first arg
    ($1) instead of forcibly prefixing the version with "smgl-". This will
    make it easier to make non-release ISOs with custom names, since
    mkrelease.sh can handle prepending "smgl-" to the version before
    passing the string to mkiso.sh for official releases.

--- scripts/mkiso.sh
+++ scripts/mkiso.sh
@@ -15,7 +15,7 @@ shift $(($OPTIND - 1))
 ISO_CHROOT=$1
 ISO_VERSION=$2
 
-mkisofs -R -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V SOURCEMAGE -o "smgl-${ISO_VERSION}.iso" "$ISO_CHROOT"
+mkisofs -R -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -V SOURCEMAGE -o "${ISO_VERSION}.iso" "$ISO_CHROOT"
 
 if [[ "$COMPRESS" == yes || -n "$KEEP" ]]
 then
