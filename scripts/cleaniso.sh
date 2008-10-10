8ca06997a8b133254157c8b58b2fd2a207370e4f
Justin Boffemmyer <flux@sourcemage.org>
2008-10-10 11:24:00 -0400

    scripts/cleaniso.sh: better error handling
    Moved the usage info out into a real function so it can be used
    multiple times. Added checks to make sure that the script is being
    called correctly, as well as to make sure that the script is operating
    on a sane chroot dir (make sure it has rm and rmdir).

--- scripts/cleaniso.sh
+++ scripts/cleaniso.sh
@@ -5,18 +5,18 @@
 CLEANALL=false
 CLEANERS="$(dirname $0)/../data/cleaners"
 
-if [[ $# -lt 2  ]]
-then
-	echo ""
-	echo "Usage: $0 iso_chroot cleaner(s)"
-	echo ""
-	echo "For each cleaner specified as an argument, cleans the"
-	echo "files and directories listed therein from iso_chroot."
-	echo "A cleaner is the ouput of 'gaze install \$SPELL' minus"
-	echo "whatever you don't want removed."
-	echo ""
+function usage() {
+	cat <<-USAGE
+	Usage: $0 [-a] iso_chroot cleaner(s)
+
+	For each cleaner specified as an argument, cleans the
+	files and directories listed therein from iso_chroot.
+	A cleaner is the ouput of 'gaze install \$SPELL' minus
+	whatever you don't want removed. If -a is specified,
+	it will use all cleaners found in the cauldron cleaner dir.
+	USAGE
 	exit 1
-fi >&2
+} >&2
 
 while getopts ":a" Option
 do
@@ -27,9 +27,35 @@ do
 done
 shift $((OPTIND - 1))
 
+# Check to make sure we have the right number of arguments
+# taking the option flag into account
+if [[ $# -eq 1 ]]
+then
+	if ! $CLEANALL
+	then
+		usage
+	fi
+elif [[ $# -lt 2 ]]
+then
+	usage
+fi
+
 # location of ISO chroot to clean from
 ISOCHROOT="$1"
 shift
+
+# Ensure sanity of chroot by making sure that it has
+# rm and rmdir.
+if ! [[ -e "$ISOCHROOT/bin/rm" ]]
+then
+	echo "Error: /bin/rm not found in $ISOCHROOT" >&2
+	exit 1
+fi
+if ! [[ -e "$ISOCHROOT/bin/rmdir" ]]
+then
+	echo "Error: /bin/rmdir not found in $ISOCHROOT" >&2
+	exit 1
+fi
 
 if $CLEANALL
 then
