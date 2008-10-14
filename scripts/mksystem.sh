9e4828438e910a2457a762e0e3d1e0d45c4c75d8
Justin Boffemmyer <flux@sourcemage.org>
2008-10-13 22:44:22 -0400

    scripts/mksystem.sh: unify opts & usage info
    The usage info is being unified across all of the cauldron scripts.
    This includes unifying the options a little as well. The
    options/arguments may be unified further, so this is not necessarily
    final.

--- scripts/mksystem.sh
+++ scripts/mksystem.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+function usage() {
+	cat << EndUsage
+Usage: $(basename $0) -d /path/to/target /path/to/output
+Generates a compressed tarball of the "system" directory specified in
+/path/to/target. This script requires superuser privileges.
+
+Required:
+	/path/to/target
+	    The path to the "system" directory.
+
+	/path/to/output
+	    The path to the desired output file.
+
+Options:
+	-v  if specified, tar progress is output to STDOUT
+
+	-h  Shows this help information
+EndUsage
+	exit 1
+} >&2
+
 while getopts ":v" Option
 do
 	case $Option in
 		v ) VERBOSE="-v" ;;
-		* ) ;;
+		h ) usage ;;
+		* ) echo "Unrecognized option." >&2 && usage ;;
 	esac
 done
 shift $(($OPTIND - 1))
@@ -15,12 +37,6 @@ SYSTAR="${2:-$(basename $SYSDIR).tar.bz2}"
 [[ $(dirname $SYSDIR) == '.' ]] && SYSDIR="$PWD/$SYSDIR"
 [[ $(dirname $SYSTAR) == '.' ]] && SYSTAR="$PWD/$SYSTAR"
 
-function usage() {
-	echo ""
-	echo "usage: mksystem /path/to/system/dir OUTPUT_FILE"
-	echo "where OUTPUT_FILE is specified via an absolute path"
-	exit 1
-}
 
 [[ -z $SYSDIR ]] && usage
 
