1a53732defd53f69a2932e88a4b282fbadebefd5
Andrew Stitt <acedit@armory.com>
2008-12-29 17:00:37 -0800

    updates

--- scripts/twelve-step-program.sh
+++ scripts/twelve-step-program.sh
@@ -8,24 +8,24 @@
   echo step 1
   cd /root
   #wget http://10.0.0.199/smgl-stable-0.24-basesystem-x86.tar.bz2 &&
-  tar xf smgl-stable-0.24-basesystem-x86.tar.bz2 &&
+  tar xvf smgl-stable-0.24-basesystem-x86.tar.bz2 &&
   mv smgl-stable-0.24-basesystem-x86 /root/build &&
   ls -l /root
   test -d /root/build || 
   echo 'step 1 failed' >> /var/log/sorcery/activity
 
   # step 1.5 (add resolv.conf and sorcery url configuration)
+  echo "step 1.5"
   cp /etc/resolv.conf /root/build/etc/resolv.conf
-  cp /etc/sorcery/local/url /root/build/etc/sorcery/local/url
+  #cp /etc/sorcery/local/url /root/build/etc/sorcery/local/url
 
   # step 2 (build spells)
   echo step 2
-  bash /root/cauldron/scripts/spellcaster.sh -c /root/build x86 ||
+  bash /root/cauldron/scripts/spellcaster.sh /root/build x86 ||
   echo 'step 2 failed' >> /var/log/sorcery/activity
 
   
-  # step 3 (build kernel)
-  echo step 3
+  echo step 3 (build kernel)
   pushd /usr/src &&
   wget http://10.0.0.11/smgl/spool/linux-2.6.24.tar.bz2 &&
   tar xf linux-2.6.24.tar.bz2 &&
@@ -41,52 +41,43 @@
   echo 'step 3 failed' >> /var/log/sorcery/activity
 
 
-  # step 4
-  echo step 4
+  echo step 4 (sanity fixes)
+  # TODO check for ppp/resolv.conf borkage
   if test -f /etc/udev/rules.d/70-persistent-net.rules; then
     echo "twelve step program failure (udev rules)" >>  /var/log/sorcery/activity &&
     rm /etc/udev/rules.d/70-persistent-net.rules
   fi ||
   echo 'step 4 failed' >> /var/log/sorcery/activity
 
-  # step 5 (create iso and system tree)
-  echo step 5
-  cp -fav build iso &&
-  cp -fav build system &&
-  bash /root/cauldron/scripts/spellcaster.sh -d system x86 ||
+  echo step 5 (adjust system tree)
+  yes ""|bash /root/cauldron/scripts/add-sauce.sh -s /tmp/cauldron/sys &&
+  # TODO: cleanse --sweep &&
+  rm /tmp/cauldron/sys/var/spool/sorcery/* &&
+  rm /tmp/cauldron/sys/var/cache/sorcery/* &&
+  rm /tmp/cauldron/sys/usr/src/* &&
+  # TODO symlink with /var/spool/sorcery &&
+  cp /usr/src/linux-2.6.24.tar.bz2 /tmp/cauldron/sys/usr/src ||
   echo 'step 5 failed' >> /var/log/sorcery/activity
 
-  # step 6 (adjust system tree)
-  echo step 6
-  yes ""|bash /root/cauldron/scripts/add-sauce.sh -s /root/system &&
-  rm -rf /root/system/var/spool/sorcery/* &&
-  rm -rf /root/system/var/cache/sorcery/* &&
-  rm -rf /root/system/usr/src/* &&
-  # TODO symlink with /var/spool/sorcery &&
-  cp /usr/src/linux-2.6.24.tar.bz2 /root/system/usr/src ||
+  echo step 6 (prune iso tree)
+  bash /root/cauldron/scripts/cleaniso.sh -a /tmp/cauldron/iso ||
   echo 'step 6 failed' >> /var/log/sorcery/activity
-  
-  # step 7 (adjust iso tree)
-  echo step 7
-  bash /root/cauldron/scripts/cleaniso.sh -a /root/iso &&
-  yes ""|bash /root/cauldron/scripts/add-sauce.sh -i /root/iso ||
+
+  echo step 7 (adjust iso tree)
+  yes ""|bash /root/cauldron/scripts/add-sauce.sh -i /tmp/cauldron/iso ||
   echo 'step 7 failed' >> /var/log/sorcery/activity
 
-  # step 8 (create system.tar.bz2)
-  echo step 8
-  mkdir -p /root/cauldron/iso/ &&
-  bash /root/cauldron/scripts/mksystem.sh /root/system /root/cauldron/iso/system.tar.bz2 ||
+  echo step 8 (create system.tar.bz2)
+  bash /root/cauldron/scripts/mksystem.sh /tmp/cauldron/sys /tmp/cauldron/iso/system.tar.bz2 ||
   echo 'step 8 failed' >> /var/log/sorcery/activity
 
-  # step 9
-  echo step 9
-  bash /root/cauldron/scripts/mkinitrd.sh /root/iso 2.6.24 &&
+  echo step 9 make initrd
+  bash /root/cauldron/scripts/mkinitrd.sh /tmp/cauldron/iso 2.6.24 &&
   cp initrd.gz /root/cauldron/iso/boot/initrd.gz ||
   echo 'step 9 failed' >> /var/log/sorcery/activity
 
-  # step 10
-  echo step 10
-  bash /root/cauldron/scripts/mkrelease.sh /root/iso omfga-test0 ||
+  echo step 10 make iso
+  bash /root/cauldron/scripts/mkrelease.sh /tmp/cauldron/iso omfga-test0 ||
   echo 'step 10 failed' >> /var/log/sorcery/activity
 
   # step 11
