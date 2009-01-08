1ddbdbd6bf2672bbb4f6a985ba0ce970c1db40d4
Justin Boffemmyer <flux@sourcemage.org>
2009-01-08 09:12:35 -0500

    scripts/cleaniso.sh: simplify for new process
    The new generation process doesn't require all the cleaning previously
    necessary, since the iso dir only starts with what we specifically put
    into it anyway. Therefore, cleaniso.sh does much less than it used to.
    Cleaner files are no longer necessary. Instead, it only cleans out a few
    specific directories of all their files, so this can be included
    directly into the script.

--- scripts/cleaniso.sh
+++ scripts/cleaniso.sh
@@ -1,33 +1,21 @@
 #/bin/bash
 
-# CLEANFILEs are located in cauldron/data/cleaners
-
-CLEANALL=false
-CLEANERS="$(dirname $0)/../data/cleaners"
-
 function usage() {
 	cat <<EndUsage
-Usage: $(basename $0) [-a] iso_chroot cleaner(s)
+Usage: $(basename $0) iso_chroot
 
-For each cleaner specified as an argument, cleans the files and
-directories listed therein from iso_chroot.  A cleaner is the ouput of
-'gaze install \$SPELL' minus whatever you don't want removed. This
-script requires superuser privileges.
+Cleans unwanted files and directories from iso_chroot. This script requires
+superuser privileges.
 
 Options:
-	-a  Run on all cleaner files in the cauldron cleaner
-	    directory. If this option is specified, any arguments
-	    provided beyond iso_chroot are ignored.
-
 	-h  Shows this help information
 EndUsage
 	exit 1
 } >&2
 
-while getopts ":ah" Option
+while getopts ":h" Option
 do
 	case $Option in
-		a ) CLEANALL=true ;;
 		h ) usage ;;
 		* ) echo "Unrecognized option." >&2 && usage ;;
 	esac
@@ -49,13 +37,7 @@ fi
 
 # Check to make sure we have the right number of arguments
 # taking the option flag into account
-if [[ $# -eq 1 ]]
-then
-	if ! $CLEANALL
-	then
-		usage
-	fi
-elif [[ $# -lt 2 ]]
+if [[ $# -ne 1 ]]
 then
 	usage
 fi
@@ -71,54 +53,18 @@ then
 	echo "Error: /bin/rm not found in $ISOCHROOT" >&2
 	exit 2
 fi
-if ! [[ -e "$ISOCHROOT/bin/rmdir" ]]
-then
-	echo "Error: /bin/rmdir not found in $ISOCHROOT" >&2
-	exit 2
-fi
 
-if $CLEANALL
-then
-	LIST=( $(ls $CLEANERS/*) )
-else
-	LIST=( $@ )
-fi
-
-for CLEANER in "${LIST[@]}"
-do
-	# Reverse sort ensures that the gaze install output we have lists files
-	# before directories, so that directories can be cleaned using rmdir
-	# after the files are cleaned first. This is safer, since it avoids the
-	# mighty 'rm -fr' oopses.
-	for DIRT in $(sort -r "$CLEANER")
-	do
-		if [[ -e $ISOCHROOT/$DIRT ]]
-		then
-			# test if current listing is a dir, should only be true after
-			# the files under the dir are already cleaned
-			if [[ -d "$DIRT" ]]
-			then
-				# chroot and clean a directory using rmdir
-				echo "Attempting to remove directory $ISOCHROOT/$DIRT"
-				chroot "$ISOCHROOT" /bin/rmdir --ignore-fail-on-non-empty "$DIRT"
-			else
-				# chroot and clean an individual file
-				echo "Attempting to delete $ISOCHROOT/$DIRT"
-				chroot "$ISOCHROOT" /bin/rm -f "$DIRT"
-			fi
-		fi
-	done
-done
+echo "Removing $ISOCHROOT/boot/grub"
+chroot "$ISOCHROOT" /bin/rm -fr /boot/grub
 
 echo "Removing $ISOCHROOT/usr/include"
-chroot /bin/rm -fr "$ISOCHROOT/usr/include"
+chroot "$ISOCHROOT" /bin/rm -fr /usr/include
 
 echo "Removing $ISOCHROOT/usr/share/doc"
-chroot /bin/rm -fr "$ISOCHROOT/usr/share/doc"
+chroot $ISOCHROOT /bin/rm -fr /usr/share/doc
 
 echo "Removing $ISOCHROOT/var/log/sorcery"
-chroot /bin/rm -fr "$ISOCHROOT/var/log/sorcery"
+chroot $ISOCHROOT /bin/rm -fr /var/log/sorcery
 
 echo "Removing $ISOCHROOT/var/state/sorcery"
-chroot /bin/rm -fr "$ISOCHROOT/var/state/sorcery"
-
+chroot $ISOCHROOT /bin/rm -fr /var/state/sorcery
